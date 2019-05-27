0aaa6cd65fb7a08747e4f118211f351615d8212a
Luis Verde Arregoitia <luis@liomys.mx>
2019-05-26 23:00:01 -0400

    new clipboard fn

---
+++ R/eval_clip_annpckgs.R
@@ -0,0 +1,14 @@
+#' Annonate clipboard content
+#'
+#' Evaluates and annotates clipboard content.
+#'
+#' @return prints annotated clipboard content to the console
+#'
+#' @export
+eval_clip_annpckgs <- function(){
+
+  clip_source <- clipr::read_clip()
+  clip_parsed <- paste0(clip_source,sep = "\n",collapse = "")
+
+  cat(annotate_pckg_calls(clip_parsed))
+}
