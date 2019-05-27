70c40635f7544d613fbc2550e4f9a2f50e3ce8d8
Luis Verde Arregoitia <luis@liomys.mx>
2019-05-27 01:02:12 -0400

    addressing missing packages

--- R/annotate_pckg_calls.R
+++ R/annotate_pckg_calls.R
@@ -4,7 +4,8 @@
 #' @param pckg_field field from package description to retrieve, defaults to
 #'   "Title"
 #'
-#' @return text string with package Title annotations
+#' @return text string with package Title annotations. Will make note of
+#'   packages not currently installed.
 #' #'
 #' @examples
 #' test_string <- c("library(boot)\nrequire(Matrix)")
@@ -15,6 +16,7 @@ annotate_pckg_calls <- function(string_og, pckg_field = "Title") {
   out_tb <- match_pckg_names(string_og)
   # get pckg titles
   out_tb$pck_desc <- purrr::map_chr(out_tb$package_name, utils::packageDescription, fields = pckg_field)
+  out_tb$pck_desc <- stringi::stri_replace_na(out_tb$pck_desc,"not installed on this machine")
   # new title variable
   out_tb$annotated <- paste(out_tb$call, "#", out_tb$pck_desc)
   stringi::stri_replace_all_fixed(
