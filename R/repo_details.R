b33e5ad9fed3e354911586487229373c96e9e52a
Luis Verde Arregoitia <luis@liomys.mx>
2019-06-09 20:58:12 -0400

    added repo helper function

---
+++ R/repo_details.R
@@ -0,0 +1,11 @@
+#' Retrieve Repo Details
+#'
+#' Internal helper function.
+#' @param pkgs_col Name of variable with the non-CRAN repos.
+#'
+#' @return A character vector of user names and repository names.
+#'
+repo_details <- function(pkgs_col) {
+  repDets <- purrr::map(pkgs_col, utils::packageDescription, fields = c("RemoteUsername", "RemoteRepo"))
+  purrr::flatten_chr(purrr::map(repDets, paste0, collapse = "/"))
+}
