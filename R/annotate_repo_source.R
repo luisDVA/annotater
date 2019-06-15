e2ad33b3a393d1a748dcd93b60495cf40be5f5d5
Luis Verde Arregoitia <luis@liomys.mx>
2019-06-15 14:26:45 -0400

    support for Bioconductor packages

--- R/annotate_repo_source.R
+++ R/annotate_repo_source.R
@@ -13,26 +13,26 @@ annotate_repo_source <- function(string_og) {
   out_tb <- match_pckg_names(string_og)
   out_tb <- tibble::rowid_to_column(out_tb)
   pck_descs <- purrr::map(out_tb$package_name, utils::packageDescription,
-                          fields = c("Repository", "RemoteType")
+    fields = c("Repository", "RemoteType", "biocViews")
   )
   pck_descs <- purrr::map(pck_descs, as.list)
   pck_descs <- tidyr::unnest(tibble::enframe(purrr::map(pck_descs, purrr::flatten_chr), name = "rowid", value = "repo"))
   pck_descs <- dplyr::left_join(out_tb, pck_descs)
+  pck_descs <- dplyr::mutate(pck_descs, repo = ifelse(stringr::str_detect(repo, ","), "Bioconductor", repo))
   pck_descs <- dplyr::add_count(pck_descs, package_name)
   pck_descs <- na.omit(dplyr::mutate(pck_descs, repo = dplyr::if_else(n == 1, "none", repo)))
-  pck_descs <- dplyr::mutate(pck_descs,
-                             user_repo =
-                               dplyr::case_when(
-                                 repo == "CRAN" ~ "CRAN",
-                                 repo == "none" ~ "not installed on this machine",
-                                 TRUE ~ repo_details(package_name)
-                               ),
-                             annotation = dplyr::case_when(
-                               stringr::str_detect(user_repo, "/") ~ paste0(repo, "::", user_repo),
-                               TRUE ~ user_repo
-                             )
-  )
-  pck_descs$annotated <- paste0(pck_descs$call, " # ", "[", pck_descs$annotation,"]")
+  pck_descs <- dplyr::mutate(pck_descs, user_repo = dplyr::case_when(
+    repo ==
+      "CRAN" ~ "CRAN",
+    repo == "Bioconductor" ~ "Bioconductor",
+    repo == "none" ~ "not installed on this machine",
+    TRUE ~ annotater:::repo_details(package_name)
+  ), annotation = dplyr::case_when(stringr::str_detect(
+    user_repo,
+    "/"
+  ) ~ paste0(repo, "::", user_repo), TRUE ~ user_repo))
+
+  pck_descs$annotated <- paste0(pck_descs$call, " # ", "[", pck_descs$annotation, "]")
   stringi::stri_replace_all_fixed(
     str = string_og, pattern = pck_descs$call,
     replacement = pck_descs$annotated, vectorize_all = FALSE
