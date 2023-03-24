e2a2843e220bc29f216365018403c295de435998
Luis Verde Arregoitia <luisd@ciencias.unam.mx>
2023-03-24 19:34:27 -0300

    new addin

---
+++ R/annotate_pkg_datasets_active.R
@@ -0,0 +1,22 @@
+#' Annotate loaded data in active file
+#'
+#' Annotates package load calls with loaded datasets in the active .R file
+#'
+#' @return No return value, wraps [annotate_pkg_datasets()] for access via Addin
+#'
+#' @export
+annotate_data_active_file <- function() {
+  context <- rstudioapi::getActiveDocumentContext()
+  if (context[1]$id == "#console") {
+    stop("Focus (blinking cursor) is not on an open R file")
+  }
+  contents_parsed <- paste0(context$contents, sep = "\n", collapse = "")
+  out <- annotater::annotate_pkg_datasets(contents_parsed)
+  outlines <- stringi::stri_split_lines1(out)
+
+  rstudioapi::modifyRange(
+    c(1, 1, length(context$contents) + 1, 1),
+    paste0(append(outlines, ""), collapse = "\n"),
+    id = context$id
+  )
+}
