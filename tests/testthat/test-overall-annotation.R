b19b49eeb87a7c7a57f055f9bf881e307d75daf8
Luis Verde Arregoitia <luisd@ciencias.unam.mx>
2024-01-25 23:32:47 -0300

    tests with fewer assumptions

--- tests/testthat/test-overall-annotation.R
+++ tests/testthat/test-overall-annotation.R
@@ -13,18 +13,18 @@ test_that("repo details returns a user/repository-name vector", {
 })
 
 test_that("repository sources returns a repository-version combination", {
-  test_string <- c("library(generics)")
+  test_string <- c("library(tibble)")
   expect_match(
     annotate_repo_source(test_string),
-    "library\\(generics\\) # .+\\w+ v.*"
+    "library\\(tibble\\) # .+\\w+ v.*"
   )
 })
 
 test_that("repository sources returns a repository-version combination (p_load)", {
-  test_string <- c("p_load(generics)")
+  test_string <- c("p_load(tibble)")
   expect_match(
     annotate_repo_source(test_string),
-    "p_load\\(\ngenerics # .+\\w+ v.*\\)"
+    "p_load\\(\ntibble # .+\\w+ v.*\\)"
   )
 })
 
@@ -33,7 +33,7 @@ test_that("input for alignment function is a character string", {
 })
 
 test_that("repository title and sources includes a repository-version combination", {
-  test_string <- c("library(generics)")
+  test_string <- c("library(tibble)")
   expect_match(
     annotate_repostitle(test_string),
     "\\w v.*"
@@ -52,8 +52,8 @@ library(stringi)"
 })
 
 test_that("package function annotations when none used", {
-  test_string <- c("library(generics)\nread_delim('dat')")
-  test_string_p <- c("p_load(generics)\nread_delim('dat')")
+  test_string <- c("library(purrr)\nread_delim('dat')")
+  test_string_p <- c("p_load(tidyr)\nread_delim('dat')")
   expect_match(
     annotate_fun_calls(test_string),
     "No used functions found"
@@ -66,7 +66,7 @@ test_that("package function annotations when none used", {
 
 
 test_that("package dataset annotations when none used", {
-  test_string <- c("library(generics)\nread_delim('dat')")
+  test_string <- c("library(tibble)\nread_delim('dat')")
   expect_match(
     annotate_pkg_datasets(test_string),
     "No loaded datasets found"
@@ -74,15 +74,15 @@ test_that("package dataset annotations when none used", {
 })
 
 test_that("if statement for pacman calls works", {
-  test_string <- c("p_load(generics)")
+  test_string <- c("p_load(purrr)")
   expect_match(
     annotate_repostitle(test_string),
-    "# Common "
+    "# Functional"
   )
 })
 
 test_that("if statement for pacman + base calls works", {
-   test_string <- c("p_load(generics)\nlibrary(tidyr)")
-expect_match(annotate_repostitle(test_string),"# Common")
-expect_match(annotate_repostitle(test_string),"# Tidy")
- })
+  test_string <- c("p_load(purrr)\nlibrary(tidyr)")
+  expect_match(annotate_repostitle(test_string), "# Functional")
+  expect_match(annotate_repostitle(test_string), "# Tidy")
+})
